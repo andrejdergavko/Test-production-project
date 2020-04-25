65d18cece2e02bd943264709ed716b1fbfa50ee7
Andrei Dergavko <andrejdergavko@gmail.com>
2020-04-25 20:55:55 +0300

    create slider component

--- client/src/components/PageContainer.jsx
+++ client/src/components/PageContainer.jsx
@@ -1,15 +1,59 @@
-import React from 'react';
-import {styled} from 'baseui';
+import React from "react";
+import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 
-import Header from './Header';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
-const Container = styled('div',{
-  margin: '40px',
-});
+const headerProps = {
+  overrides: {
+    Block: {
+      style: () => ({
+        width: "100%",
+        flexGrow: 0,
+      }),
+    },
+  },
+};
 
-export default ({children}) => (
-  <div>
-    <Header />
-    <Container>{children}</Container>
-  </div>
-);
+const sidebarProps = {
+  overrides: {
+    Block: {
+      style: () => ({
+        width: "100px",
+        flexGrow: 0,
+      }),
+    },
+  },
+};
+
+export default ({ children }) => {
+  return (
+    // <FlexGrid
+    //   flexGridColumnCount={2}
+    //   flexGridColumnGap={0}
+    //   flexGridRowGap={0}
+    //   marginBottom={0}
+    // >
+    //   <FlexGridItem {...headerProps}>
+    //     <Header />
+    //   </FlexGridItem>
+
+    //   <FlexGridItem display="none" />
+
+    //   <FlexGridItem {...sidebarProps}>
+    //     <Sidebar />
+    //   </FlexGridItem>
+
+    //   <FlexGridItem>{children}</FlexGridItem>
+    // </FlexGrid>
+
+    <div>
+      <Header />
+
+      <div>
+        <div style={{display: 'inline-block'}}><Sidebar /></div>
+        <div style={{display: 'inline-block'}}>{children}</div>
+      </div>
+    </div>
+  );
+};
